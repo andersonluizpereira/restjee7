9c7b3ba9a1b53619f32324e572cd1660afb0f1ae
anderson luiz pereira <andy2903.alp@gmail.com>
2020-03-25 15:17:10 -0300

    resolved search cep

--- src/br/com/ws/rest/example1/CepRestWS.java
+++ src/br/com/ws/rest/example1/CepRestWS.java
@@ -2,7 +2,9 @@ package br.com.ws.rest.example1;
 
 import java.util.Random;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -53,6 +55,18 @@ public class CepRestWS {
 	       Webservicecep wCep = (Webservicecep) u.unmarshal( url );
 	       
 	       return wCep;
+    }
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("setCep")
+    public void setCep(Cep cep) {
+    	System.out.println("Objeto Cep Recebido ! - "+cep);
+    	System.out.println("Cep "+cep.getCep());
+    	System.out.println("Rua "+cep.getRua());
+    	System.out.println("Bairro "+cep.getBairro());
+    	System.out.println("Numero "+cep.getNumero());
+    	System.out.println("UF "+cep.getUf());
     }
 	
 }
